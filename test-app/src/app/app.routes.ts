209c41124fe4e4c9b4c8a0d41d22cef13147c485
aishwinder <aishwinder.sandhu@gmail.com>
2025-05-05 12:23:36 +0100

    Login Component | Created form and added body structure

--- test-app/src/app/app.routes.ts
+++ test-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //to route login component
+  {path:'login',component:LoginComponent},
+  //For default routing or on page load, redirect to login Page 
+  {path:'**', component:LoginComponent},
+
+];
