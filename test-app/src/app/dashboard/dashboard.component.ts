6af13814056e1a9925a41dc83ba41e582b794404
aishwinder <aishwinder.sandhu@gmail.com>
2025-05-07 22:26:28 +0100

    Dashboard | Form Implementation in progress

--- test-app/src/app/dashboard/dashboard.component.ts
+++ test-app/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { FormService } from '../services/form.service';
+import {FormBuilder,ReactiveFormsModule,FormGroup, Validators} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   title = "User successfully logged in";
-  constructor(private formService: FormService) { }
+  auditForm : FormGroup;
+  constructor(private formService: FormService, private fb:FormBuilder) { 
+    this.auditForm = this.fb.group({
+      ward: ['', Validators.required],//Validation for Ward as required
+      text_input: ['',Validators.required]
+    })
+  }
+ 
+  //fetching audit form data as soon as the user is authenticated
+  //populating some fields based on the data received
   ngOnInit(): void {
     this.formService.getFormData()
       .subscribe({
@@ -17,15 +29,12 @@ export class DashboardComponent {
           console.log(data, "data");
         },
         error: (err) => {
-          console.log(err, "err in getForms");
+          console.log(err, "err in GET AUDIT FORMS");
         }
       });
-     
-      
   }
   submitForm(){
-    
-    this.formService.submitFormData()
+    this.formService.submitFormData(this.auditForm.value)
       .subscribe({
         next:(data)=>{
           console.log("submitted successfully", data);
--- test-app/src/app/services/form.service.ts
+++ test-app/src/app/services/form.service.ts
@@ -11,8 +11,8 @@ interface AuditFormResponse {
 })
 export class FormService {
   id = 96;//Ideally need to pass the id acquired from user profile from logging-in
-  // private getFormDataUrl = `/api/v2/audit_form/${this.id}/`;
-  private getFormDataUrl = `/api/v2/audit_form/`;
+  private getFormDataUrl = `/api/v2/audit_form/${this.id}/`;
+  // private getFormDataUrl = `/api/v2/audit_form/`;
   private postFormDataUrl = `/api/v2/audit_form/${this.id}/submit/`;
   //getting authenticated token from localstorage, this can be done via session as well. 
   token = localStorage.getItem('token');
@@ -23,26 +23,7 @@ export class FormService {
       'Authorization': `Token ${this.token}`, //sending the obtained token for further api calls.
     }
   }
-  postObj = {
-      "start_time": "2025-05-07T17:49:00.000Z",
-      "end_time": "2025-05-07T17:49:00.000Z",
-      "comment": null,
-      "send_email": false,
-      "observations": [
-        {
-          "ward": 32,
-          "date": "2025-05-07T17:49:40.262Z",
-          "issues": [],
-          "answer_comments": [],
-          "valid": true,
-          "text_input": "abcd",
-          "choice_field": "N/A"
-        }
-      ],
-      "signatures": [],
-      "report_auditors": [],
-      "submitting_anonymously": false
-  }
+ 
 
   constructor(private http: HttpClient) { }
 
@@ -60,8 +41,28 @@ export class FormService {
   post_headers = {
     headers: new HttpHeaders().set('Authorization',`Token ${this.token}`)
   }
-  submitFormData():Observable<any>{
-    return this.http.post<any>(this.postFormDataUrl,this.postObj,this.post_headers).pipe(
+  submitFormData(auditForm:{ward: string,text_input: string }):Observable<any>{
+    var postObj = {
+      "start_time": "2025-05-07T17:49:00.000Z",
+      "end_time": "2025-05-07T17:49:00.000Z",
+      "comment": null,
+      "send_email": false,
+      "observations": [
+        {
+          "ward": auditForm.ward,//updated Form to be sent with user values
+          "date": "2025-05-07T17:49:40.262Z",
+          "issues": [],
+          "answer_comments": [],
+          "valid": true,
+          "text_input": auditForm.text_input,
+          "choice_field": "N/A"
+        }
+      ],
+      "signatures": [],
+      "report_auditors": [],
+      "submitting_anonymously": false
+  }
+    return this.http.post<any>(this.postFormDataUrl,postObj,this.post_headers).pipe(
       tap((res)=>{
         console.log("res",res);
       }),
